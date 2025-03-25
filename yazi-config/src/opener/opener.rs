9fd7ceb7dd258aa354be17833a5ca50b8727e4c2
Sami Farin <hvtaifwkbgefbaei@gmail.com>
2025-03-25 18:39:51 +0200

    fix Misaki's force pushed commit

--- yazi-config/src/opener/opener.rs
+++ yazi-config/src/opener/opener.rs
@@ -45,16 +45,7 @@ impl Opener {
 			*rules = mem::take(rules)
 				.into_iter()
 				.map(|mut r| (r.for_.take(), r))
-<<<<<<< HEAD
-				.filter(|(for_, _)| match for_.as_ref().map(|s| s.as_str()) {
-					Some("unix") if cfg!(unix) => true,
-					Some(os) if os == std::env::consts::OS => true,
-					Some(_) => false,
-					None => true,
-				})
-=======
 				.filter(|(for_, _)| check_for(for_.as_deref()))
->>>>>>> upstream/main
 				.map(|(_, r)| r.reshape())
 				.collect::<Result<IndexSet<_>>>()?
 				.into_iter()
